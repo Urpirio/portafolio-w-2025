da21a59bf1ed6a4beb3557284586141d997aab48
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-03-17 06:27:25 -0400

    Se agrego un array de datos para HoverNav

---
+++ src/app/Data/DataHoverNav.js
@@ -0,0 +1,20 @@
+'use client'
+const DataHoverNav = [
+    {
+        Nav: 'Home',
+        id: 'HoverHome',
+    },
+    {
+        Nav: 'About',
+        id: 'HoverAbout',
+    },
+    {
+        Nav: 'Proyects',
+        id: 'HoverProyects',
+    },
+    {
+        Nav: 'Contacts',
+        id: 'HoverContact',
+    }
+];
+export default DataHoverNav;
