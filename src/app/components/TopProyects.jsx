ac06a9dbe70a1be1faaa232c88111234be4a19c0
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-03-17 08:03:58 -0400

    Se agrego el btn de ver mas

--- src/app/components/TopProyects.jsx
+++ src/app/components/TopProyects.jsx
@@ -14,8 +14,14 @@ export default function TopProyects() {
     });
 
   return (
-    <div className='flex flex-wrap justify-center gap-5 p-10'>
+    <div className='flex flex-col items-center  gap-10 p-5'>
+        <div className='flex flex-wrap justify-center gap-5 w-[90%]'>
         {TopListProyects}
+        </div>
+        <div>
+            <button className='flex justify-center items-center h-10 p-2 border-1 rounded-[10px] font-bold bg-green-500 text-white cursor-pointer transition-all duration-200 hover:translate-y-[-10px] hover:opacity-75'>See more projects</button>
+        </div>
+        
     </div>
   )
 }
