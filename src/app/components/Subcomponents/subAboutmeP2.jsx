23620dacb0c1349c443f0602979cccbae78be801
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-03-17 06:26:20 -0400

    Se agrego subcomponente de aboutme

---
+++ src/app/components/Subcomponents/subAboutmeP2.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function SubAboutmeP2(props) {
+  return (
+    <button className='h-10 w-25 shadow-1xl text-gray-700 bg-gray-300 rounded-sm  cursor-pointer hover:opacity-75'>
+        {props.Typebtn}
+    </button>
+  )
+}
