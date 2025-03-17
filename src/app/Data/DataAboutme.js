75167ee23edd1f30fada0055673179703db93f3d
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-03-17 06:27:11 -0400

    Se agrego un array de datos para Aboutme

---
+++ src/app/Data/DataAboutme.js
@@ -0,0 +1,47 @@
+const DataAboutme = [
+    {
+        Type: 'Html',
+        Content: '',
+    },
+    {
+        Type: 'CSS',
+        Content: '',
+    },
+    {
+        Type: 'React',
+        Content: '',
+    },
+    {
+        Type: 'React Native',
+        Content: '',
+    },
+    {
+        Type: 'Next.js',
+        Content: '',
+    },
+    {
+        Type: 'Node.js',
+        Content: '',
+    },
+    {
+        Type: 'MySQL',
+        Content: '',
+    },
+    {
+        Type: 'Tailwind',
+        Content: '',
+    },
+    {
+        Type: 'Java Scripts',
+        Content: '',
+    },
+    {
+        Type: 'Less',
+        Content: '',
+    },
+    {
+        Type: 'Sass',
+        Content: '',
+    },
+];
+export default DataAboutme;
