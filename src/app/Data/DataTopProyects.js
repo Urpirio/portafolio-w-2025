34bfbf6f6c784d58dff9a7c856c8f87fbfb562a6
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-03-17 06:27:46 -0400

    Se agrego un array de datos para Top proyects

---
+++ src/app/Data/DataTopProyects.js
@@ -0,0 +1,141 @@
+import DragonFinder from './../../../public/Img-Proyects/DragonFinder-2.png';
+import DragonFinder2 from './../../../public/Img-Proyects/DragonFinder.png';
+import clon_Instagram from './../../../public/Img-Proyects/Clon-Instagram.png';
+import clon_Instagram2 from './../../../public/Img-Proyects/Clon-Instagram-2.png';
+import GobHome from './../../../public/Img-Proyects/GOBHome.png';
+import GobHome2 from './../../../public/Img-Proyects/GOBHome2.png';
+import Portafolio_web_2024  from './../../../public/Img-Proyects/PortafolioWeb-2024.png';
+import Portafolio_web_2024_2  from './../../../public/Img-Proyects/PortafolioWeb-2024 (2).png';
+import travelPage from './../../../public/Img-Proyects/TravelPage.png';
+import travelPage2 from './../../../public/Img-Proyects/TravelPage-2.png';
+import Urpsoft from './../../../public/Img-Proyects/Urpsoft.png';
+import Urpsoft2 from './../../../public/Img-Proyects/Urpsoft2.png';
+import Urpspace from './../../../public/Img-Proyects/UrpSpace.png';
+import ListWeb from './../../../public/Img-Proyects/ListWeb.png';
+import ListWeb2 from './../../../public/Img-Proyects/ListWeb2.png';
+import FirstPortafolio from './../../../public/Img-Proyects/FirstPortafolio.png';
+import FirstPortafolio2 from './../../../public/Img-Proyects/FirstPortafolio2.png';
+
+
+
+
+const DataTopProyects = [
+    {
+        Img: [
+            {
+                D: DragonFinder,
+            },
+            {
+                D: DragonFinder2,
+            }
+        ],
+        NameP: 'Dragon Finder',
+        Descripcion: '',
+
+    },
+    {
+        Img: [
+            {
+                D: clon_Instagram,
+            },
+            {
+                D: clon_Instagram2,
+            }
+        ],
+        NameP: 'Clon Instagram (In development)',
+        Descripcion: '',
+
+    },
+    {
+        Img: [
+            {
+                D: GobHome,
+            },
+            {
+                D: GobHome2,
+            }
+        ],
+        NameP: 'Gob.do',
+        Descripcion: '',
+
+    },
+    {
+        Img: [
+            {
+                D: Portafolio_web_2024,
+            },
+            {
+                D: Portafolio_web_2024_2,
+            }
+        ],
+        NameP: 'Portafolio-Web-2024',
+        Descripcion: '',
+
+    },
+    {
+        Img: [
+            {
+                D: travelPage,
+            },
+            {
+                D: travelPage2,
+            }
+        ],
+        NameP: 'Travel Page',
+        Descripcion: '',
+
+    },
+    {
+        Img: [
+            {
+                D: Urpsoft,
+            },
+            {
+                D: Urpsoft2,
+            }
+        ],
+        NameP: 'Urpsoft',
+        Descripcion: '',
+
+    },
+    {
+        Img: [
+            {
+                D: Urpspace,
+            },
+            {
+                D: '',
+            }
+        ],
+        NameP: 'Urpspace',
+        Descripcion: '',
+
+    },
+    {
+        Img: [
+            {
+                D: ListWeb2,
+            },
+            {
+                D: ListWeb,
+            }
+        ],
+        NameP: 'List Web (In development)',
+        Descripcion: '',
+
+    },
+    {
+        Img: [
+            {
+                D: FirstPortafolio,
+            },
+            {
+                D: FirstPortafolio2,
+            }
+        ],
+        NameP: 'First portfolio',
+        Descripcion: '',
+
+    },
+];
+export default DataTopProyects;
