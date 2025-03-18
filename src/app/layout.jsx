11b1e77bb00c145ccf7b00af683d2d456725e524
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-03-17 22:17:49 -0400

    Se agrego el footer al layout

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -27,7 +27,7 @@ export default function Layout({children}) {
           <Image src={Logo} className='h-10 w-10'/>
           <h1 className='font-bold text-2xl'>Dev</h1>
         </div>
-        <nav className='flex items-center justify-center gap-5 ml-10  text-[17px] '>
+        <nav className='items-center hidden justify-center gap-5 ml-5  text-[17px] sm:flex '>
           <div onMouseEnter={HoverNav} onMouseLeave={HoverNav} className='cursor-pointer transition-all duration-300 hover:text-green-500'>
             Home
             <div className='h-1' id='HoverHome'></div>
@@ -40,16 +40,36 @@ export default function Layout({children}) {
             Proyects
             <div className='h-1' id='HoverProyects'></div>
           </div>
-          <div onMouseEnter={HoverNav4} onMouseLeave={HoverNav4} className='cursor-pointer transition-all duration-300 hover:text-green-500'>
+          {/* <div onMouseEnter={HoverNav4} onMouseLeave={HoverNav4} className='cursor-pointer transition-all duration-300 hover:text-green-500'>
             Contacts
             <div className='h-1' id='HoverContact'></div>
-          </div>
+          </div> */}
         </nav>
         <div className=''>
           <button className=' flex h-10 p-5  border-1 justify-center items-center rounded-2xl shadow-2xl shadow-green-200 bg-green-400 text-white font-bold  '>Contact</button>
         </div>
       </header>
         {children}
+        <footer className='flex flex-col ' >
+          <section className='flex pb-5 pt-5 gap-5  sm:p-5  justify-around bg-gray-900 flex-wrap'>
+            <div className='flex flex-col w-[30%] min-w-80 text-center sm:text-left  gap-2'>
+              <h3 className='text-2xl font-bold text-green-500'>Urpirio Junior</h3>
+              <span className='text-gray-200'>A Frontend focused Web Developer building the Frontend of Websites
+                 and Web Applications that leads to the success of the overall product</span>
+            </div>
+            <div className='flex flex-col w-[30%] min-w-80 items-center gap-2 '>
+              <h3 className='text-2xl font-bold text-green-500'>Social networks</h3>
+              <div className='flex gap-5'>
+                <i class='bx bxl-linkedin-square text-4xl text-white cursor-pointer transition-all duration-200 hover:scale-110 hover:text-green-500'></i>
+                <i class='bx bxl-instagram-alt text-4xl text-white cursor-pointer transition-all duration-200 hover:scale-110 hover:text-green-500 ' ></i>
+                <i class='bx bxl-github text-4xl text-white cursor-pointer transition-all duration-200 hover:scale-110 hover:text-green-500' ></i>
+              </div>
+            </div>
+          </section>
+          <section className='flex justify-center items-center text-center p-5 text-white text-xl font-bold border-t-1 bg-gray-900'>
+            <span>Copyright © 2025 Urpsoft. Todos los derechos reservados.</span>
+          </section>
+        </footer>
     </body>
     </html>
   )
