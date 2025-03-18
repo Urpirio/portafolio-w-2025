f2d342d1471c0b15eb1546bb509f91020b21efad
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-03-18 13:25:13 -0400

    Se agrego un nuevo funcion

---
+++ src/app/func/LinkLinkedin.js
@@ -0,0 +1,6 @@
+'use client';
+
+const LinkLinkedin = () => {
+    window.location.href = 'https://www.linkedin.com/in/urpirio-junior-moreno-vargas-95294b269/?originalSubdomain=do';
+};
+export default LinkLinkedin;
