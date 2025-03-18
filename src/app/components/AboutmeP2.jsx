118ef590415a120d86ef9ff7b5fde51d895ba8ed
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-03-18 09:17:39 -0400

    Update correccion de errores

--- src/app/components/AboutmeP2.jsx
+++ src/app/components/AboutmeP2.jsx
@@ -11,23 +11,23 @@ export default function AboutmeP2() {
     })
 
   return (
-    <section className='flex flex-wrap justify-center p-10 gap-20'>
-        <div className='flex flex-col gap-2 p-5 min-w-100 w-[40%] max-w-180'>
-            <div>
+    <section className='flex flex-wrap items-center sm:justify-around  '>
+        <div className='flex flex-col  min-w-90 w-[40%] max-w-180 p-5 '>
+            <div className='mb-5'>
                 <h2 className='font-bold text-2xl m-0'>Get to Know me!</h2>
             </div>
-            <div>
-                <p className='text-[16px] pb-2 pt-2'>I'm a Frontend Focused Web Developer building and managing
+            <div className=''>
+                <p className='text-[16px] mb-5 '>I'm a Frontend Focused Web Developer building and managing
                     the Front-end of Websites and Web Applications that leads
                     to the success of the overall product. Check out some of my
                     work in the Projects section.</p>
-                <p className='text-[16px] pb-2 pt-2'>
+                <p className='text-[16px] mb-5'>
                 I also like sharing content related to the stuff that I have learned over the years
                  in Web Development so it can help other people of the Dev Community. Feel free to
                   Connect or Follow me on my <a href="" className='text-green-400 hover:border-b-1'>Linkedin</a> and <a href="" className='text-green-400 hover:border-b-1'>Instagram </a> 
                   where I post useful content related to Web Development and Programming.
                 </p>
-                <p className='text-[16px] pb-2 pt-2'>
+                <p className='text-[16px] mb-5'>
                  I'm open to Job opportunities where I can contribute, learn and grow. If you have
                  a good opportunity that matches my skills and experience then don't hesitate to contact me.
                 </p>
@@ -36,7 +36,7 @@ export default function AboutmeP2() {
                 <button className='flex items-center justify-center h-10 border-1 w-50 rounded-sm bg-green-500 text-white font-bold shadow-2xl transition-all duration-300 cursor-pointer hover:opacity-80'>Contact</button>
             </div>
         </div>
-        <div className=' flex flex-col items-center min-w-100 w-[40%] max-w-180  p-5 gap-[22]'>
+        <div className=' flex flex-col items-center min-w-90 w-[40%] max-w-180  p-5 gap-[22]'>
             <div className='w-[100%]'>
                 <h2 className='font-bold text-2xl m-0' >My Skills</h2>
             </div>
