461c21ea7f24135a2c2c89d3f514733946dde4d0
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-03-18 19:15:59 -0400

    Se agrego un array de datos para el componente de servicio

---
+++ src/app/Services/Data/DataCardServices.js
@@ -0,0 +1,21 @@
+const DataCardServices = [
+    {
+        ServiceName: 'SimplePage',
+        DescriptionService: 'A simple, single-page website with smooth navigation, responsive design, and basic SEO optimization.',
+        StandarPrice: 'US$ 110',
+        id_Caracteristicas_Services: 'Card1',
+    },
+    {
+        ServiceName: 'MultiPage',
+        DescriptionService: 'A multi-page static website with clear navigation, professional design, and SEO-friendly structure.',
+        StandarPrice: 'US$ 220',
+        id_Caracteristicas_Services: 'Card2',
+    },
+    {
+        ServiceName: 'DynamicPage',
+        DescriptionService: 'An interactive, database-driven website with user authentication, dynamic content, and custom features.',
+        StandarPrice: 'US$ 500',
+        id_Caracteristicas_Services: 'Card3',
+    }
+];
+export default DataCardServices;
