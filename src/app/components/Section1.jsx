91eadb5c0e969fb928d2803aca1db2100f549358
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-03-17 22:17:17 -0400

    Se hicieron cambios para mejorar la responsividad

--- src/app/components/Section1.jsx
+++ src/app/components/Section1.jsx
@@ -11,18 +11,18 @@ export default function Section1() {
 
 
   return (
-    <section className=' flex flex-col  items-start  gap-10 p-20 bg-white  relative z-0'>
-        <div className='absolute flex flex-col rounded-2xl border-gray-300 border-1 gap-2 p-1 bg-white shadow-2xl shadow-green-100 '>
+    <section className=' flex flex-col  items-start  gap-10 p-5 sm:p-20 bg-white  relative z-0'>
+        <div className='absolute hidden flex-col rounded-2xl border-gray-300 border-1 gap-2 p-1 bg-white shadow-2xl shadow-green-100 sm:flex'>
             <i className='bx bxl-linkedin-square text-5xl text-green-500 cursor-pointer transition-all duration-300 hover:opacity-75'></i>
             <i className='bx bxl-github text-5xl text-green-500 cursor-pointer transition-all duration-300 hover:opacity-75' ></i>
             <i className='bx bxl-instagram-alt text-5xl text-green-500 cursor-pointer transform-all duration-300 hover:opacity-75' ></i>
         </div>
     <div className='w-[100%] flex justify-center items-center'>
-        <Image src={PerfilDev} className='h-80 w-80 rounded-[100%] '/>
+        <Image src={PerfilDev} className='h-80 min-w-80 w-[100%] max-w-80 rounded-[100%] '/>
     </div>
-    <div className='flex flex-col items-center gap-2'>
-      <h1 className=' font-extrabold text-5xl'>Hey, I'm  Urpirio Junior</h1>
-      <span className=' text-2xl text-center text-gray-600'>A Result-Oriented Web Developer building and managing
+    <div className='flex flex-col items-center w-[100%] gap-2'>
+      <h1 className=' font-extrabold text-4xl sm:text-5xl text-center'>Hey, I'm  Urpirio Junior</h1>
+      <span className=' text-xl sm:text-2xl text-gray-600 text-center'>A Result-Oriented Web Developer building and managing
          Websites and Web Applications that leads to the success of the overall product</span>
     </div>
     <div className='w-[100%] flex justify-center items-center'>
