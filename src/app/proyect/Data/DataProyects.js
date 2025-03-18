c839e681dafae5624dc604f4df1ca7f66f9d6c86
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-03-17 22:18:24 -0400

    Se agrego el array de datos de poyectos

---
+++ src/app/proyect/Data/DataProyects.js
@@ -0,0 +1,206 @@
+import DragonFinder from './../../../../public/Img-Proyects/DragonFinder-2.png';
+import DragonFinder2 from './../../../../public/Img-Proyects/DragonFinder.png';
+import clon_Instagram from './../../../../public/Img-Proyects/Clon-Instagram.png';
+import clon_Instagram2 from './../../../../public/Img-Proyects/Clon-Instagram-2.png';
+import GobHome from './../../../../public/Img-Proyects/GOBHome.png';
+import GobHome2 from './../../../../public/Img-Proyects/GOBHome2.png';
+import Portafolio_web_2024  from './../../../../public/Img-Proyects/PortafolioWeb-2024.png';
+import Portafolio_web_2024_2  from './../../../../public/Img-Proyects/PortafolioWeb-2024 (2).png';
+import travelPage from './../../../../public/Img-Proyects/TravelPage.png';
+import travelPage2 from './../../../../public/Img-Proyects/TravelPage-2.png';
+import Urpsoft from './../../../../public/Img-Proyects/Urpsoft.png';
+import Urpsoft2 from './../../../../public/Img-Proyects/Urpsoft2.png';
+import Urpspace from './../../../../public/Img-Proyects/UrpSpace.png';
+import ListWeb from './../../../../public/Img-Proyects/ListWeb.png';
+import ListWeb2 from './../../../../public/Img-Proyects/ListWeb2.png';
+import FirstPortafolio from './../../../../public/Img-Proyects/FirstPortafolio.png';
+import FirstPortafolio2 from './../../../../public/Img-Proyects/FirstPortafolio2.png';
+import Kfc_rd from './../../../../public/Img-Proyects/Kfc-Rd.png';
+import Kfc_rd2 from './../../../../public/Img-Proyects/Kfc-Rd2.png';
+import SimplyCompany from './../../../../public/Img-Proyects/SimplyCompany.png';
+import SimplyCompany2 from './../../../../public/Img-Proyects/SimplyCompany2.png';
+import GameRock from './../../../../public/Img-Proyects/GameRock.png';
+
+const DataProyects = [
+    {
+        Img: [
+            {
+                D: DragonFinder,
+            },
+            {
+                D: DragonFinder2,
+            }
+        ],
+        NameP: 'Dragon Finder',
+        Descripcion: 'Find your favorite Dragon Ball character.',
+        id: 'P1',
+        Boleano: false,
+
+    },
+    {
+        Img: [
+            {
+                D: clon_Instagram,
+            },
+            {
+                D: clon_Instagram2,
+            }
+        ],
+        NameP: 'Clone-Instagram (In development)',
+        Descripcion: 'Explore what a person can do.',
+        id: 'P2',
+        Boleano: false,
+
+    },
+    {
+        Img: [
+            {
+                D: GobHome,
+            },
+            {
+                D: GobHome2,
+            }
+        ],
+        NameP: 'Gob.do',
+        Descripcion: 'Helping to improve services at GOB points on Rd.',
+        id: 'P3',
+        Boleano: false,
+
+    },
+    {
+        Img: [
+            {
+                D: Portafolio_web_2024,
+            },
+            {
+                D: Portafolio_web_2024_2,
+            }
+        ],
+        NameP: 'Portafolio-Web-2024',
+        Descripcion: 'Second website in React and second web portfolio.',
+        id: 'P4',
+        Boleano: false,
+
+    },
+    {
+        Img: [
+            {
+                D: travelPage,
+            },
+            {
+                D: travelPage2,
+            }
+        ],
+        NameP: 'Travel Page',
+        Descripcion: 'Page for a tourism company.',
+        id: 'P5',
+        Boleano: false,
+
+    },
+    {
+        Img: [
+            {
+                D: Urpsoft,
+            },
+            {
+                D: Urpsoft2,
+            }
+        ],
+        NameP: 'Urpsoft',
+        Descripcion: 'My small business website',
+        id: 'P6',
+        Boleano: false,
+    },
+    {
+        Img: [
+            {
+                D: Urpspace,
+            },
+            {
+                D: '',
+            }
+        ],
+        NameP: 'Urpspace',
+        Descripcion: 'login to feel in space.',
+        id: 'P7',
+        Boleano: false,
+
+    },
+    {
+        Img: [
+            {
+                D: ListWeb2,
+            },
+            {
+                D: ListWeb,
+            }
+        ],
+        NameP: 'List Web (In development)',
+        Descripcion: 'With this you can manage your projects',
+        id: 'P8',
+        Boleano: false,
+
+    },
+    {
+        Img: [
+            {
+                D: FirstPortafolio,
+            },
+            {
+                D: FirstPortafolio2,
+            }
+        ],
+        NameP: 'First portfolio',
+        Descripcion: 'My first big project.',
+        id: 'P9',
+        Boleano: false,
+
+    },
+    {
+        Img: [
+            {
+                D: Kfc_rd,
+            },
+            {
+                D: Kfc_rd2,
+            }
+        ],
+        NameP: 'Clone-KFC',
+        Descripcion: 'Clone of the official KFC website in Rd.',
+        id: 'P10',
+        Boleano: false,
+
+    },
+    {
+        Img: [
+            {
+                D: SimplyCompany,
+            },
+            {
+                D: SimplyCompany2,
+            }
+        ],
+        NameP: 'Simply Company',
+        Descripcion: 'A simple page for a company.',
+        id: 'P11',
+        Boleano: false,
+
+    },
+    {
+        Img: [
+            {
+                D: GameRock,
+            },
+            {
+                D: '',
+            }
+        ],
+        NameP: 'Rock paper scissors game',
+        Descripcion: 'A game to have fun.',
+        id: 'P12',
+        Boleano: false,
+
+    },
+
+];
+export default DataProyects;
