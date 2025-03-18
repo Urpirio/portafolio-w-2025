ef6fce18ae438dab78079168bb7ddb86198d348d
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-03-18 19:16:12 -0400

    Se agrego un array de datos para los servicios

---
+++ src/app/Services/Data/DataFeatureService.js
@@ -0,0 +1,47 @@
+const DataFeatureService = [
+    {
+        SimplePage: [
+            {
+                service: 'Attractive and responsive design.',
+            },
+            {
+                service: 'Basic SEO optimization.',
+            },
+            {
+                service: 'Contact forms.',
+            },
+            {
+                service: 'Smooth animations and interactive effects.',
+            }
+        ],
+        MultiPage: [
+            {
+                service: 'Design of multiple static pages (home, services, contact, etc.).',
+            },
+            {
+                service: 'Easy-to-use navigation.',
+            },
+            {
+                service: 'Basic SEO optimization.',
+            },
+            {
+                service: 'Responsive design.',
+            }
+        ],
+        DynamicPage: [
+            {
+                service: 'Development with backend (Node.js).',
+            },
+            {
+                service: 'Integration with databases (MySQL).',
+            },
+            {
+                service: 'Interactive features (login, comments, purchases).',
+            },
+            {
+                service: 'Advanced SEO optimization and security.',
+            }
+        ],
+    }
+];
+export default DataFeatureService;
