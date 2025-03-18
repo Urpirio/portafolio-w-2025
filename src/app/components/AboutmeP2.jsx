004c6402de82c90495d9946dd8fdfecde32d8eb0
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-03-18 13:20:55 -0400

    Combios en la informacion

--- src/app/components/AboutmeP2.jsx
+++ src/app/components/AboutmeP2.jsx
@@ -11,29 +11,29 @@ export default function AboutmeP2() {
     })
 
   return (
-    <section className='flex flex-wrap  sm:justify-around  '>
+    <section className='flex flex-wrap  sm:justify-around  ' >
         <div className='flex flex-col  min-w-90 w-[40%] max-w-180 p-5 '>
             <div className='mb-5'>
                 <h2 className='font-bold text-2xl m-0'>Get to Know me!</h2>
             </div>
             <div className=''>
-                <p className='text-[16px] mb-5 '>I'm a Frontend Focused Web Developer building and managing
-                    the Front-end of Websites and Web Applications that leads
-                    to the success of the overall product. Check out some of my
-                    work in the Projects section.</p>
-                <p className='text-[16px] mb-5'>
-                I also like sharing content related to the stuff that I have learned over the years
-                 in Web Development so it can help other people of the Dev Community. Feel free to
-                  Connect or Follow me on my <a href="" className='text-green-400 hover:border-b-1'>Linkedin</a> and <a href="" className='text-green-400 hover:border-b-1'>Instagram </a> 
-                  where I post useful content related to Web Development and Programming.
+                <p className='text-[16px] mb-5 '>I'm a Full Stack Developer with over a year of experience building
+                modern and scalable web applications. I work with technologies like React, Next.js, JavaScript, 
+                Tailwind, and Sass to create intuitive, fast, and responsive interfaces.
                 </p>
                 <p className='text-[16px] mb-5'>
-                 I'm open to Job opportunities where I can contribute, learn and grow. If you have
-                 a good opportunity that matches my skills and experience then don't hesitate to contact me.
+                On the backend, I use Node.js and MySQL to manage servers, databases, and API integrations, ensuring
+                efficient and secure performance. My focus is on building complete applications that deliver a great
+                user experience.
+                </p>
+                <p className='text-[16px] mb-5'>
+                I'm always learning and sharing knowledge with the tech community. Passionate about developing 
+                innovative and optimized solutions. If you're looking for a versatile and dedicated developer,
+                let's connect! 🚀
                 </p>
             </div>
             <div>
-                <button className='flex items-center justify-center h-10 border-1 w-50 rounded-sm bg-green-500 text-white font-bold shadow-2xl transition-all duration-300 cursor-pointer hover:opacity-80'>Contact</button>
+                <button className='flex items-center justify-center h-10 border-1 w-50 rounded-xl bg-green-500 text-white font-bold shadow-2xl transition-all duration-300 cursor-pointer hover:opacity-80'>Get in Touch!</button>
             </div>
         </div>
         <div className=' flex flex-col items-center min-w-90 w-[40%] max-w-180  p-5 gap-[22]'>
