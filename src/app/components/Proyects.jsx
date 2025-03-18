7e4eedd0874f5d0bda7f137e95f5554665a6b468
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-03-18 13:21:46 -0400

    Se agrego un id

--- src/app/components/Proyects.jsx
+++ src/app/components/Proyects.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 
 export default function Proyects() {
   return (
-    <section className='flex flex-col items-center p-10'>
+    <section className='flex flex-col items-center p-10' >
         <div className='flex flex-col items-center gap-3 '>
             <h2 className=' text-4xl'>Proyects</h2>
             <div className='h-1.5  bg-green-400 w-7 rounded-2xl'></div>
         </div>
-        <div className='flex items-center justify-center'>
+        <div className='flex items-center justify-center' id='TopProyect'>
             <span className='text-[20px] text-center'>
             Here you will find some of the personal and clients
             projects that I created with each project containing its own case study
