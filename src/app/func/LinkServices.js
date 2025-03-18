7f96efe4a56fcfce581437dbd3db54be8ca517da
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-03-18 19:14:54 -0400

    Agrego una nueva funcion

---
+++ src/app/func/LinkServices.js
@@ -0,0 +1,10 @@
+'use client';
+
+// import DataCardServices from "../Services/Data/DataCardServices";
+// import DataFeatureService from "../Services/Data/DataFeatureService";
+
+const LinkServices = () => {
+    window.location.href = '/Services';
+
+};
+export default LinkServices;
