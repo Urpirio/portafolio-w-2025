f8b344334c0b229161c3bdd6b4070ec337649915
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-03-18 09:42:24 -0400

    Se mejoro la responsividad

--- src/app/proyect/page.jsx
+++ src/app/proyect/page.jsx
@@ -21,7 +21,7 @@ export default function Proyects() {
   return (
     <section className='flex flex-wrap p-10 gap-5 justify-center items-center'>
       <div className='w-[100%] flex justify-center'>
-        <span className='flex items-center justify-center border-1 border-gray-400 rounded-2xl h-10 w-[50%]  min-w-100 gap-1 '>
+        <span className='flex items-center justify-center border-1 border-gray-400 rounded-2xl h-10 w-[50%]  min-w-80 gap-1 '>
           <i className='bx bx-search text-2xl text-gray-400' ></i>
           <input onInput={Search} id="Search1" className='w-[90%] h-[100%]  pl-2 pr-2 outline-none ' type="text" name=""  placeholder='Search proyect'/>
           <i className='bx bx-x text-2xl' style={{display: 'none'}}></i>
