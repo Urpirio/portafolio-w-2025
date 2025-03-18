310467599dfd0281f99261ecb97cf9d6fc106ea0
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-03-18 13:25:34 -0400

    Se hicieron mejoras

--- src/app/func/LinkProyects.js
+++ src/app/func/LinkProyects.js
@@ -1,5 +1,9 @@
 'use client';
-const Linkproyect = () => {
-    window.location.href = '/proyect';
+const Linkproyect = (Pro) => {
+    if(Pro == false){
+        window.location.href = '#TopProyect';
+    }else{
+        window.location.href = '/proyect'
+    }
 };
 export default Linkproyect;
