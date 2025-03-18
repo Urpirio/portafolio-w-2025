6b00bbdc47f29750e99fb6b40c95a7c78ca3b9a4
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-03-18 13:20:36 -0400

    No existe cambios

--- src/app/components/Aboutme.jsx
+++ src/app/components/Aboutme.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Aboutme() {
   return (
-    <section className='flex flex-col border-t-1 border-gray-200 justify-center items-center p-5'>
+    <section className='flex flex-col border-t-1 border-gray-200 justify-center items-center p-5' >
         <div className='flex flex-col items-center gap-2'>
             <h2 className='text-4xl'>About me</h2>
             <div className='h-1.5 w-7 bg-green-400 rounded-2xl'></div>
