2e5c314e525cb2108e6a6fc96c50c7a609c40901
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-03-18 19:15:35 -0400

    Se agrego un nuevo componente de servicios

---
+++ src/app/Services/components/CardServices.jsx
@@ -0,0 +1,25 @@
+'use client';
+import React from 'react';
+
+
+
+export default function CardServices(props) {
+
+  return (
+   <article className=' h-100 w-[25%] min-w-80 flex flex-col items-center justify-center gap-5 p-5 border-1 border-gray-300 rounded-3xl bg-gray-50 shadow-xl'>
+    <div className='flex flex-col w-[100%] gap-2'>
+        <h2 className='m-0 text-3xl text-green-500 font-bold'>{props.ServiceName}</h2>
+        <p className='m-0 text-gray-500'>{props.DescriptionService}</p>
+    </div>
+    <div className='w-[100%]'>
+        <span className='text-green-700 text-xl font-bold'>{props.StandarPrice}</span>
+    </div>
+    <div className='flex flex-col w-[100%] text-2xs text-gray-800' id={props.id_Caracteristicas_Services}>
+        
+    </div>
+    <div className='w-[100%] flex justify-center '>
+        <button className='h-10 w-[100%] border-1 rounded-2xl text-white bg-green-500'>Get service</button>
+    </div>
+   </article>
+  )
+}
