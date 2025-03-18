bd91a2abcd6f73eca7b69fd6a8264066e5998825
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-03-18 13:22:15 -0400

    Se agregaron Tres funciones nuevas y se cambio la informacion

--- src/app/components/Section1.jsx
+++ src/app/components/Section1.jsx
@@ -1,7 +1,11 @@
+'use client';
 import React from 'react';
 import Image from 'next/image';
 import Linkproyect from '../func/LinkProyects';
 import PerfilDev from '../../../public/image.png';
+import LinkLinkedin from '../func/LinkLinkedin';
+import LinkGithud from '../func/LinkGithud';
+import LinkInstagram from '../func/LinkInstagram';
 
 
 
@@ -13,20 +17,20 @@ export default function Section1() {
   return (
     <section className=' flex flex-col  items-start  gap-10 p-5 sm:p-20 bg-white  relative z-0'>
         <div className='absolute hidden flex-col rounded-2xl border-gray-300 border-1 gap-2 p-1 bg-white shadow-2xl shadow-green-100 sm:flex'>
-            <i className='bx bxl-linkedin-square text-5xl text-green-500 cursor-pointer transition-all duration-300 hover:opacity-75'></i>
-            <i className='bx bxl-github text-5xl text-green-500 cursor-pointer transition-all duration-300 hover:opacity-75' ></i>
-            <i className='bx bxl-instagram-alt text-5xl text-green-500 cursor-pointer transform-all duration-300 hover:opacity-75' ></i>
+            <i onClick={LinkLinkedin} className='bx bxl-linkedin-square text-5xl text-green-500 cursor-pointer transition-all duration-300 hover:opacity-75'></i>
+            <i onClick={LinkGithud} className='bx bxl-github text-5xl text-green-500 cursor-pointer transition-all duration-300 hover:opacity-75' ></i>
+            <i onClick={LinkInstagram} className='bx bxl-instagram-alt text-5xl text-green-500 cursor-pointer transform-all duration-300 hover:opacity-75' ></i>
         </div>
     <div className='w-[100%] flex justify-center items-center'>
         <Image src={PerfilDev} className='h-80 min-w-80 w-[100%] max-w-80 rounded-[100%] '/>
     </div>
     <div className='flex flex-col items-center w-[100%] gap-2'>
       <h1 className=' font-extrabold text-4xl sm:text-5xl text-center'>Hey, I'm  Urpirio Junior</h1>
-      <span className=' text-xl sm:text-2xl text-gray-600 text-center'>A Result-Oriented Web Developer building and managing
-         Websites and Web Applications that leads to the success of the overall product</span>
+      <p className=' text-xl sm:text-2xl text-gray-600 text-center'>A results-driven Web Developer
+      focused on building and optimizing websites and web applications to enhance user experience and drive product success.</p>
     </div>
-    <div className='w-[100%] flex justify-center items-center'>
-      <button onClick={Linkproyect} className='bg-green-400 shadow-3xl h-12 w-40 text-white rounded-sm text-[18px] cursor-pointer transition-all duration-300 hover:opacity-75'>Proyects</button>
+    <div className='w-[100%] flex justify-center items-center' id='Aboutme'>
+      <button onClick={()=>{Linkproyect(false)}} className='bg-green-500 shadow-3xl h-12 w-40 text-white rounded-xl text-[18px] cursor-pointer transition-all duration-300 hover:opacity-75'>Proyects</button>
     </div>
   </section>
   )
