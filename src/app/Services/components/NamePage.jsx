1676066ae6662a22d089eab08b32288e540ee618
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-03-19 19:30:29 -0400

    Se agrego un componente solo para moviles

---
+++ src/app/Services/components/NamePage.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function NamePage() {
+  return (
+    <div className='flex flex-col items-center p-10 font-semibold text-4xl sm:hidden'>
+        <h1>Services</h1>
+        <div className='bg-green-500 rounded-2xl h-1.5 w-5'></div>
+    </div>
+  )
+}
