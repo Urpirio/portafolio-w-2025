89cda601cc65ca96c0498615388c317385bc17db
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-03-19 17:19:42 -0400

    Se agrego la funcion de deploy form

---
+++ src/app/Services/components/Subcomponents/func/DeployForm.js
@@ -0,0 +1,21 @@
+'use client';
+import DataCardServices from "@/app/Services/Data/DataCardServices";
+
+
+const DeployForm = () => {
+    const FormGetServices = document.getElementById('FormGetServices');
+    const PageServices = document.getElementById('PageServices');
+    if(screen.width < 667 ){
+        FormGetServices.style.display = 'flex';
+        FormGetServices.style.position = 'relative';
+        FormGetServices.style.width = '80px';
+        FormGetServices.style.borderRadius ='20px';
+        for(let x = 0; x < DataCardServices.length; x++){
+            document.getElementById(`C${x}`).style.display = 'none';
+        };
+    }else{
+        PageServices.style.padding = '200px'
+        FormGetServices.style.display = 'flex';
+    };
+};
+export default DeployForm;
