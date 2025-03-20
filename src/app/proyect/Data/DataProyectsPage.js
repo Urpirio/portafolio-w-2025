3895aa00dd38b094f0ad97457ee103920bb22005
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-03-20 13:31:50 -0400

    Se agrego un array de datos para identificar las pages

---
+++ src/app/proyect/Data/DataProyectsPage.js
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+const DataProyectsPage = [
+    {
+        Page: '1',
+        NextPage: '2',
+        LinkPage: '/proyect',
+        LinkNextPage: '/proyect/page2',
+    },
+];
+export default DataProyectsPage;
