37ced4bd45cd0c1c45b7e6962c13d521d30349ee
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-03-20 13:29:29 -0400

    Se agregaron los botones de navegacion inferior

---
+++ src/app/proyect/components/Subcomponents/BtnNextPage.jsx
@@ -0,0 +1,29 @@
+'use client';
+
+import React from 'react';
+import { usePathname } from 'next/navigation';
+import DataProyectsPage from '../../Data/DataProyectsPage';
+
+// let Direction;
+export default function BtnNextPage() {
+    const Gpath = usePathname()
+    const path = DataProyectsPage[0].LinkPage === Gpath;
+    const path2 = DataProyectsPage[0].LinkNextPage === Gpath;   
+
+    const Direction = () => {
+        if(path === true){
+            window.location.href = '/proyect/page2';
+        }else{
+
+        }
+    };
+
+  return (
+    <div>
+         <button onClick={Direction} className={`h-6 w-6 flex justify-center items-center border-1  rounded-xs shadow-xl cursor-pointer transition-all duration-200 hover:opacity-75 
+            ${path2 ? 'bg-gray-200 border-gray-200 text-gray-600' : 'bg-green-500 text-white' }`} id='NextProyect'>
+            <i class='bx bx-chevron-right'></i>
+        </button>
+    </div>
+  )
+}
