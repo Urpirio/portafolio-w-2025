0f8c64ed7bafdade9ef716382bc7d030ded6f4b4
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-03-20 13:31:30 -0400

    Se agrego el array de datos de la nueva pagina de proyectos

---
+++ src/app/proyect/Data/DataProyects2.js
@@ -0,0 +1,104 @@
+import G4Sanvalentine from './../../../../public/Img-Proyects2/G4SanValentine.png'
+import ClonLoginFacebook from './../../../../public/Img-Proyects2/ClonLoginFacebook.png';
+import DragonFanPage from './../../../../public/Img-Proyects2/DragonFanPage.png';
+import DragonFanPage2 from './../../../../public/Img-Proyects2/DragonFanPage2.png';
+import FirstPageReact from './../../../../public/Img-Proyects2/FirstPageReact.png';
+import FirstPageReact2 from './../../../../public/Img-Proyects2/FirstPageReact2.png';
+import TechnicalTestIntro from './../../../../public/Img-Proyects2/TechnicalTestIntro.png';
+import TechnicalTestIntro2 from './../../../../public/Img-Proyects2/TechnicalTestIntro2.png';
+import ScienceC from './../../../../public/Img-Proyects2/ScienceC.png';
+
+
+const DataProyects2 = [
+    {
+            Img: [
+                {
+                    D: G4Sanvalentine,
+                },
+                {
+                    D: '',
+                }
+            ],
+            NameP: `G4 Valentine's Day`,
+            Descripcion: 'Send a letter to your favorite person.',
+            id: 'P1',
+            Boleano: false,
+    
+   },
+   {
+    Img: [
+        {
+            D: ClonLoginFacebook,
+        },
+        {
+            D: '',
+        }
+    ],
+    NameP: 'Clon-Login-Facebook',
+    Descripcion: `It's the same.`,
+    id: 'P2',
+    Boleano: false,
+
+},
+{
+    Img: [
+        {
+            D: DragonFanPage,
+        },
+        {
+            D: DragonFanPage2,
+        }
+    ],
+    NameP: 'Dragon fan page',
+    Descripcion: 'For the Dragon Ball community to unite.',
+    id: 'P3',
+    Boleano: false,
+
+},
+{
+    Img: [
+        {
+            D: FirstPageReact,
+        },
+        {
+            D: FirstPageReact2,
+        }
+    ],
+    NameP: 'First page react',
+    Descripcion: 'My first page using react.',
+    id: 'P4',
+    Boleano: false,
+
+},
+{
+    Img: [
+        {
+            D: TechnicalTestIntro,
+        },
+        {
+            D: TechnicalTestIntro2,
+        }
+    ],
+    NameP: 'Intro technical test',
+    Descripcion: 'Only Html, Css and JavaScript.',
+    id: 'P5',
+    Boleano: false,
+
+},
+{
+    Img: [
+        {
+            D: ScienceC,
+        },
+        {
+            D: '',
+        }
+    ],
+    NameP: 'Scientific calculator',
+    Descripcion: 'Phase 1 of the calculator.',
+    id: 'P6',
+    Boleano: false,
+
+},
+]
+export default DataProyects2;
