3dd41e6057f2588fdb1fd9d754c1e199c362603b
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-03-21 05:02:13 -0400

    Se agrego una funcion envia el path

--- src/app/proyect/components/SearchProyects.jsx
+++ src/app/proyect/components/SearchProyects.jsx
@@ -1,12 +1,15 @@
+'use client'
 import React from 'react';
 import Search from '../func/Search';
+import { usePathname } from 'next/navigation';
 
 export default function SearchProyects() {
+  const path = usePathname();
   return (
     <div className='w-[100%] flex justify-center'>
         <span className='flex items-center justify-center border-1 border-gray-400 rounded-2xl h-10 w-[50%]  min-w-80 gap-1 '>
           <i className='bx bx-search text-2xl text-gray-400' ></i>
-          <input onInput={Search} id="Search1" className='w-[90%] h-[100%]  pl-2 pr-2 outline-none ' type="text" name=""  placeholder='Search proyect'/>
+          <input onInput={()=>{Search(path)}} id="Search1" className='w-[90%] h-[100%]  pl-2 pr-2 outline-none ' type="text" name=""  placeholder='Search proyect'/>
           <i className='bx bx-x text-2xl' style={{display: 'none'}}></i>
         </span>
     </div>
