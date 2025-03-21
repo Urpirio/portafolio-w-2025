6ad121e1dff2fbe76298eecab87ad0d1284cfedc
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-03-21 05:02:53 -0400

    Se agrego h1 para los moviles

--- src/app/proyect/page.jsx
+++ src/app/proyect/page.jsx
@@ -7,6 +7,10 @@ export default function Proyects() {
 
   return (
     <section className='flex flex-wrap p-10 gap-5 justify-center items-center'>
+      <div className='flex flex-col justify-center items-center gap-2 text-4xl font-semibold sm:hidden'>
+        <h1>Proyects</h1>
+        <div className='h-2 w-5 bg-green-500 rounded-2xl '></div>
+      </div>
      <SearchProyects/>
      <CardProyects/>
      <NavProyects/>
