e9b3d6284877d568c5ce3c4941622142c9bda350
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-03-23 16:07:59 -0400

    Se agregaron nuevas clases y funciones

--- src/app/components/Contact.jsx
+++ src/app/components/Contact.jsx
@@ -1,9 +1,13 @@
 'use client';
 
-import React from 'react';
+import React, { Suspense } from 'react';
 import LinkMyContact from '../func/LinkMyContact';
+import ValueInputHome from '../func/Email/ValueInputHome';
+import SendtoVerifyEmail from '../func/Email/SendtoVerifyEmail';
+import SendtoSoport from '../func/Email/SendtoSoport';
 
 export default function Contact() {
+
   return (
     <section className='flex flex-col gap-10 p-10' id='Contact'>
         <div className='flex flex-col items-center justify-center'>
@@ -16,34 +20,36 @@ export default function Contact() {
             </div>
         </div>
         <div className='flex flex-wrap justify-center items-center sm:justify-around w-[100%] gap-10'>
-            <form action="" className='flex flex-col w-[30%] min-w-80  gap-2 p-10 border-2 border-green-500  shadow-xl rounded-2xl'>
-                <div>
+            <form action="" onSubmit={(e)=>{e.preventDefault(); SendtoVerifyEmail() }} className='flex flex-col w-[30%] min-w-80  gap-2 p-10 border-2 border-green-500  shadow-xl rounded-2xl' id='FormHome'>
+                <div id='F1'>
                     <h2 className='text-[25px] font-semibold text-green-900'>Send me a message</h2>
                 </div>
-                <div className='flex flex-col'>
+                <div className='flex flex-col' id='F2'>
                     <label htmlFor="">Name</label>
-                    <input className='h-10 p-1 outline-none border-1 border-gray-400 rounded-md' type="text" placeholder='Your name' required/>
+                    <input onInput={ValueInputHome} className='h-10 p-1 outline-none border-1 border-gray-400 rounded-md  InputHome1' type="text" id='nameFormHome' placeholder='Your name' required/>
                 </div>
-                <div className='flex flex-col'>
+                <div className='flex flex-col' id='F3'>
                     <label htmlFor="">Email</label>
-                    <input className='h-10 p-1 outline-none border-1 border-gray-400 rounded-md' type="email" placeholder='Your email' required/>
+                    <input onInput={ValueInputHome} className='h-10 p-1 outline-none border-1 border-gray-400 rounded-md InputHome2' type="email" id='emailFormHome' placeholder='Your email' required/>
                 </div>
-                <div className='flex flex-col'>
+                <div className='flex flex-col' id='F4'>
                     <label htmlFor="">Phone number</label>
-                    <input className='h-10 p-1 outline-none border-1 border-gray-400 rounded-md' type="number" placeholder='Your number' required/>
+                    <input onInput={ValueInputHome} className='h-10 p-1 outline-none border-1 border-gray-400 rounded-md InputHome3' type="number" id='numberFormHome' placeholder='Your number' required/>
                 </div>
-                <div className='flex flex-col'>
+                <div className='flex flex-col' id='F5'>
                     <label htmlFor="">Company</label>
-                    <input className='h-10 p-1 outline-none border-1 border-gray-400 rounded-md' type="text" placeholder='Name of your company' required/>
+                    <input onInput={ValueInputHome} className='h-10 p-1 outline-none border-1 border-gray-400 rounded-md InputHome4' type="text" id='companyFormHome' placeholder='Name of your company' required/>
                 </div>
-                <div className='flex flex-col'>
+                <div className='flex flex-col' id='F6'>
                     <label htmlFor="">Message</label>
-                    <textarea className='h-20 p-1 outline-none border-1 resize-none border-gray-400 rounded-md' name="" id="" placeholder='Tell me about your project...' required></textarea>
+                    <textarea onInput={ValueInputHome} className='h-20 p-1 outline-none border-1 resize-none border-gray-400 rounded-md InputHome5' id="aboutProyectFormHome" placeholder='Tell me about your project...' required></textarea>
                 </div>
-                <div>
+                <div id='F7'>
                     <button className=' w-30 text-[18px] text-white font-bold border-1  p-1 rounded-md bg-green-500 cursor-pointer hover:opacity-75' type='submit'>Send</button>
                 </div>
+
             </form>
+
             <div className='flex flex-col justify-center border-2 border-green-500 shadow-2xl bg-white rounded-2xl p-5 gap-10'>
                 <div onClick={()=>{LinkMyContact(false)}} className='flex flex-col items-center transition-all duration-200 cursor-pointer hover:scale-110'>
                     <i className='bx bxl-whatsapp  text-5xl text-white rounded-[100%] h-14 w-14 bg-green-400' style={{display: 'flex', justifyContent: 'center', alignItems: 'center'}}></i>
