ff384279d744fe8419b03bae44ed81611f39ba22
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-03-23 16:02:21 -0400

    Se agrego el mensaje que confirma que se resivio la informacion

---
+++ emails/MessageClient.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export default function MessageClient(props) {
+  return (
+    <div>
+        <h2>Hello, {props.name}</h2>
+        <p>We have successfully received your information and will begin processing it. Your request will be processed within 24 hours.</p>
+        <br />
+        <p>If you need to make any changes or have any questions, please don't hesitate to contact us.</p>
+        <br />
+        <p>Thank you for trusting UrpirioDev.</p>
+        <br />
+        <p>Sincerely, Human resources
+            Urpiriodev.online
+            Udev@urpiriodev.online
+            +1(829)-586-2316
+        </p>
+    </div>
+  )
+}
