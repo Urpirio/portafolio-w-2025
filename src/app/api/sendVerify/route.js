e341e4f4d8c9b855485600e7fc6f2418eac11eba
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-03-23 16:06:13 -0400

    Se agrego un conexion a  la api para enviar el codigo de confirmacion

---
+++ src/app/api/sendVerify/route.js
@@ -0,0 +1,19 @@
+import { Resend } from "resend";
+import ConfirmacionEmail from "../../../../emails/ConfirmacionEmail";
+
+const resend = new Resend('re_Ytjrd1Ks_5FkyBAJ4naso1CzdNAqPxzVL');
+
+export async function POST(request){
+        const {Email,CodeV} = await request.json();
+        await resend.emails.send({
+
+            from: 'Udev <urpiriodev@urpiriodev.online>',
+            to: Email,
+            subject: 'Email confirmation code',
+            react:<ConfirmacionEmail
+            CodeV = {CodeV}
+            />,
+            text: '',
+        });
+
+};
