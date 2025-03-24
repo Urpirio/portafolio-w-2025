0ed941f5fb7e7c655bdab6b5f8f6dfaa557f450b
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-03-24 18:34:43 -0400

    Se re estructura ciertas funciones

--- src/app/components/Subcomponents/ContactAndMenu.jsx
+++ src/app/components/Subcomponents/ContactAndMenu.jsx
@@ -2,23 +2,18 @@
 
 import React from 'react';
 import { usePathname } from 'next/navigation';
-import { useState } from 'react';
-import Deploymenu from './func/Deploymenu';
 import LinkContact from '@/app/func/LinkContact';
+import BtnMenu from './func/BtnMenu';
 
-export default function ContactAndMenu() {
-    const pathBoleano = usePathname();
-    const [ ChangeClass, SetChangeClass] = useState(false);
 
-    const DeployMenu = () => {
-        SetChangeClass(!ChangeClass);
-        Deploymenu(ChangeClass);
-    };
+export default function ContactAndMenu() {//componente del menu
+  
+  const pathBoleano = usePathname();
 
   return (
     <div className=''>
         <button onClick={()=>{LinkContact(pathBoleano)}} className=' hidden sm:flex h-10 p-5  border-1 justify-center items-center rounded-2xl shadow-2xl shadow-green-200 bg-green-400 text-white font-bold cursor-pointer hover:opacity-75 '>Contact</button>
-        <button onClick={DeployMenu} className='text-4xl sm:hidden flex'><i className={ ChangeClass ? 'bx bx-x' : 'bx bx-menu' } ></i></button>
+        <button onClick={BtnMenu} className='text-4xl sm:hidden flex'><i className='bx bx-menu' id='IconMenu' ></i></button>
     </div>
   )
 }
