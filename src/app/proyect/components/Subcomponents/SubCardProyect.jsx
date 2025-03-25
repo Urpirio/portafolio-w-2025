a5963f26b5431c54973b430f290197a1fbd8765a
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-03-24 21:29:27 -0400

    Mejoras en el estilo movile

--- src/app/proyect/components/Subcomponents/SubCardProyect.jsx
+++ src/app/proyect/components/Subcomponents/SubCardProyect.jsx
@@ -6,8 +6,8 @@ import LinkDeploy from '../../func/LinkProyects/LinkDeploy';
 export default function SubCardProyect(props) {
   return (
     <article className='flex flex-col gap-2 shadow-xl border-2 border-gray-300 rounded-xl ' id={props.id}>
-        <div className='w-80'>
-            <Image src={props.img} className='h-40 w-[100%] sm:w-80 sm:h-50 border-b-1 border-gray-200 rounded-t-xl'/>
+        <div className='w-80 sm:w-[100%] sm:min-w-100'>
+            <Image src={props.img} className='h-40 sm:w-[100%] w-80 sm:h-50 border-b-1 border-gray-200 rounded-t-xl'/>
         </div>
         <div className='p-2'>
             <h2 className='font-bold'>{props.NameP}</h2>
@@ -15,7 +15,7 @@ export default function SubCardProyect(props) {
         </div>
         <div className='w-[100%]  p-2 flex justify-center gap-2'>
             <button onClick={()=>{LinkDeploy(props.LinkDeploy)}} className=' h-10 w-[85%] border-1 rounded-xl bg-green-500 text-white shadow-2xs cursor-pointer transition-all duration-200 hover:opacity-75'>See project</button>
-            <button onClick={()=>{LinkGithud(props.LinkGithud)}} className='h-10 w-[15%] border-1 rounded-xl bg-gray-900 text-white shadow-2xs cursor-pointer text-2xl transition-all duration-200 hover:opacity-75'><i class='bx bxl-github'></i></button>
+            <button onClick={()=>{LinkGithud(props.LinkGithud)}} className='flex justify-center items-center h-10 w-[15%] border-1 rounded-xl bg-gray-900 text-white shadow-2xs cursor-pointer text-2xl transition-all duration-200 hover:opacity-75'><i class='bx bxl-github'></i></button>
         </div>
     </article>
   )
