baaa81a9130839da5bb264458f3355ea67d617ad
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-03-27 11:40:59 -0400

    Se modifico el ID

--- src/app/Services/Data/DataCardServices.js
+++ src/app/Services/Data/DataCardServices.js
@@ -4,21 +4,21 @@ const DataCardServices = [
         DescriptionService: 'A simple, single-page website with smooth navigation, responsive design, and basic SEO optimization.',
         StandarPrice: 'US$ 110',
         id_Caracteristicas_Services: 'Card1',
-        CardID: 'C0'
+        CardID: 'CardServices0'
     },
     {
         ServiceName: 'MultiPage',
         DescriptionService: 'A multi-page static website with clear navigation, professional design, and SEO-friendly structure.',
         StandarPrice: 'US$ 220',
         id_Caracteristicas_Services: 'Card2',
-        CardID: 'C1',
+        CardID: 'CardServices1',
     },
     {
         ServiceName: 'DynamicPage',
         DescriptionService: 'An interactive, database-driven website with user authentication, dynamic content, and custom features.',
         StandarPrice: 'US$ 500',
         id_Caracteristicas_Services: 'Card3',
-        CardID: 'C2',
+        CardID: 'CardServices2',
     }
 ];
 export default DataCardServices;
