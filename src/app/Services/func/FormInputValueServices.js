7c002dce0aa0a40894aadd59325fbeb68a99926c
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-03-27 11:44:45 -0400

    Se mejoro la funcion de enviar valores

--- src/app/Services/func/FormInputValueServices.js
+++ src/app/Services/func/FormInputValueServices.js
@@ -3,17 +3,14 @@ export let ServicesNameF;
 export let ServicesEmailF;
 export let ServicesPnumberF;
 export let ServicesCompanyF;
-export let ServicesSelected;
 export let ServicesAboutProyect;
 
-const FormInputValueServices = (Serviceselected) => {
+const FormInputValueServices = () => {
 
   ServicesNameF = document.getElementById('NameF').value;
   ServicesEmailF = document.getElementById('EmailF').value;
   ServicesPnumberF = document.getElementById('PnumberF').value;
   ServicesCompanyF = document.getElementById('CompanyF').value;
-  ServicesSelected = Serviceselected;
-  console.log(ServicesSelected);
   ServicesAboutProyect =  document.getElementById('AboutProyect').value;
 
 };
