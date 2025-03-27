6d482011c416cc6f2369eb542e8ea123bebe8e98
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-03-27 11:44:05 -0400

    Se agrego un funcion que se ejecuta como el loanding a lo que se realiza la conexion con el servidor

---
+++ src/app/Services/func/Email/Subfunc/LoadingFormServices.js
@@ -0,0 +1,38 @@
+
+const LoadinFormServices = (State = true) => {
+    if(State === true){
+        let IsLoading;
+        const divLoading = document.createElement('div');
+        divLoading.id = 'divLoading';
+        divLoading.style.width = '100%';
+        divLoading.style.display = 'flex';
+        divLoading.style.justifyContent = 'center';
+        const spanLoading = document.createElement('span');
+        spanLoading.style.fontSize = '20px';
+        spanLoading.style.fontWeight = '600';
+        spanLoading.style.color = '#0d542b';
+        divLoading.append(spanLoading);
+
+        var AnimeLoading = setInterval(()=>{
+            if(IsLoading === undefined){
+                spanLoading.innerText = 'Loading.';
+                IsLoading = 1;
+
+            }else if(IsLoading === 1){
+                spanLoading.innerText = 'Loading..';
+                IsLoading = 2;
+
+            }else{
+                spanLoading.innerText = 'Loading...';
+                IsLoading = undefined;
+            }
+        },300);
+
+        document.getElementById('ServicesForm').append(divLoading)
+
+    }else{
+        document.getElementById('divLoading').remove();
+        AnimeLoading = null;
+    }
+};
+export default LoadinFormServices;
