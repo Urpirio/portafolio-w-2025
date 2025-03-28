64eb1ba19604b7406dfc3031985cbd3745859f98
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-03-28 09:38:25 -0400

    Pequeñas mejoras en el estilo del header

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -24,25 +24,24 @@ export default function Header() {
     const pathBoleano = usePathname();
 
   return (
-    <header className='flex flex-col justify-center  p-2  bg-white border-1 border-gray-100 shadow-green-50 shadow-2xl sticky  top-[0] z-3' id='Header'>
-        <div className='flex items-center justify-between w-[100%]'>
-            <div onClick={LinkHome} className='flex justify-center items-center cursor-pointer'>
+    <header className='flex justify-center  p-2  bg-white border-1 border-gray-100 shadow-green-50 shadow-2xl sticky  top-[0] z-3' id='Header'>
+            <div onClick={LinkHome} className='flex justify-start flex-grow basis-0 items-center cursor-pointer'>
                 <Image src={Logo} alt='logo' className='h-10 w-10'/>
                  <h1 className='font-bold text-2xl'>Dev</h1>
             </div>
-            <nav className='items-center hidden justify-center gap-5 ml-5  text-[17px] sm:flex '>
+            <nav className='items-center hidden justify-center gap-5 text-[17px] sm:flex '>
           
                 <div onMouseEnter={HoverNav} onMouseLeave={HoverNav} onClick={LinkHome} 
                     className={`cursor-pointer transition-all duration-300 hover:text-green-500 
                     ${pathBoleano === '/' ? 'text-green-500' : ''} `} >
                     Home
-                    <div className='h-1' id='HoverHome'></div>
+                    <div className='h-1 rounded-2xl' id='HoverHome'></div>
                 </div>
 
                 <div onMouseEnter={HoverNav2} onMouseLeave={HoverNav2} onClick={()=>{LinkAboutMe(pathBoleano)}} 
                     className='cursor-pointer transition-all duration-300 hover:text-green-500'>
-                    About
-                    <div className='h-1 transition-all duration-300' id='HoverAbout'></div>
+                    About me
+                    <div className='h-1 rounded-2xl' id='HoverAbout'></div>
                 </div>
 
                 <div onMouseEnter={HoverNav3} onMouseLeave={HoverNav3} onClick={()=>{Linkproyect(true,pathBoleano)}} 
@@ -50,20 +49,18 @@ export default function Header() {
                     ${pathBoleano === '/proyect' ? 'text-green-500' : 
                     pathBoleano ==='/proyect/page2' ? 'text-green-500' : '' }`}>
                     Proyects
-                    <div className='h-1' id='HoverProyects'></div>
+                    <div className='h-1 rounded-2xl' id='HoverProyects'></div>
                 </div>
 
                 <div onMouseEnter={HoverNav4} onMouseLeave={HoverNav4} onClick={LinkServices} 
                 className={`cursor-pointer transition-all duration-300 hover:text-green-500 
                 ${pathBoleano === '/Services' ? 'text-green-500' : '' }`}>
                     Services
-                    <div className='h-1' id='HoverContact'></div>
+                    <div className='h-1 rounded-2xl' id='HoverContact'></div>
                 </div>
 
             </nav>
             <ContactAndMenu/>
-        </div>
-        
       </header>
   )
 }
