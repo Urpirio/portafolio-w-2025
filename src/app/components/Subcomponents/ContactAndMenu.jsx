efca2cd186221acd7217d920242bdbd73eb0983e
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-03-28 09:38:58 -0400

    Se pequeñas mejoras del header tambien aplican en esras parte

--- src/app/components/Subcomponents/ContactAndMenu.jsx
+++ src/app/components/Subcomponents/ContactAndMenu.jsx
@@ -11,7 +11,7 @@ export default function ContactAndMenu() {//componente del menu
   const pathBoleano = usePathname();
 
   return (
-    <div className=''>
+    <div className='flex flex-grow basis-0 justify-end' >
         <button onClick={()=>{LinkContact(pathBoleano)}} className=' hidden sm:flex h-10 p-5  border-1 justify-center items-center rounded-2xl shadow-2xl shadow-green-200 bg-green-400 text-white font-bold cursor-pointer hover:opacity-75 '>Contact</button>
         <button onClick={BtnMenu} className='text-4xl sm:hidden flex'><i className='bx bx-menu' id='IconMenu' ></i></button>
     </div>
