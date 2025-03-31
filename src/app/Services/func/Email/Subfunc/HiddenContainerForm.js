a73731435e4a92ccb4c3b1b279abedcb396e38a2
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-03-31 19:54:43 -0400

    Se agrego una funcion de estilo

--- src/app/Services/func/Email/Subfunc/HiddenContainerForm.js
+++ src/app/Services/func/Email/Subfunc/HiddenContainerForm.js
@@ -2,6 +2,8 @@
 const HiddenContainerForm =() => {
     for(let i = 1; i < 9; i++){
         document.getElementById(`SF${i}`).style.display = 'none';
-    }
+    };
+    document.getElementById('FormGetServices').style.marginBottom = '50px';
+    document.getElementById('FormGetServices').style.marginTop = '50px';
 };
 export default HiddenContainerForm;
