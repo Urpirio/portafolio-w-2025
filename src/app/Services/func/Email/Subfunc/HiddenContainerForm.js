ff4e9116e0bd97ee2d5312f1536fa9982503f221
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-03-31 19:58:44 -0400

    Update Error

--- src/app/Services/func/Email/Subfunc/HiddenContainerForm.js
+++ src/app/Services/func/Email/Subfunc/HiddenContainerForm.js
@@ -3,7 +3,9 @@ const HiddenContainerForm =() => {
     for(let i = 1; i < 9; i++){
         document.getElementById(`SF${i}`).style.display = 'none';
     };
-    document.getElementById('FormGetServices').style.marginBottom = '50px';
-    document.getElementById('FormGetServices').style.marginTop = '50px';
+    if(screen.width < 667){
+        document.getElementById('FormGetServices').style.marginBottom = '50px';
+        document.getElementById('FormGetServices').style.marginTop = '50px';
+    };
 };
 export default HiddenContainerForm;
