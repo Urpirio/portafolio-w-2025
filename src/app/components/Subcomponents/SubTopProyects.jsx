d633cbea98c84e8ea63f02a642821a5827ae879f
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-04-09 08:42:22 -0400

    Se mejoro el estilo

--- src/app/components/Subcomponents/SubTopProyects.jsx
+++ src/app/components/Subcomponents/SubTopProyects.jsx
@@ -12,7 +12,7 @@ export default function SubTopProyects(props) {
 
   return (
     <div onClick={LinkProyects} className='flex flex-col justify-center items-center w-100 p-2 gap-3  border-2  border-green-400 rounded-2xl shadow-2xl  cursor-pointer transition-all duration-100 hover:shadow-green-200  hover:scale-105'>
-        <Image className='h-50 w-[100%] rounded-[12px]' src={props.Image} alt={props.NameP}/>
+        <Image className='h-50 w-[100%] rounded-xl object-cover ' src={props.Image} alt={props.NameP}/>
         <div>
             <h3 className='text-green-950 font-bold'>{props.NameP}</h3>
             <span>{props.description}</span>
