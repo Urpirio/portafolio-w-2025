b1e49927078e562b4ce695e9df7c35796e658525
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-04-13 09:39:10 -0400

    Este es el antiguo DataAboutme

---
+++ src/app/Data/DataSkills.js
@@ -0,0 +1,57 @@
+export const DataSkills = [
+    {
+        key: 1 ,
+        Type: 'Html',
+        Content: '',
+    },
+    {
+        key: 2 ,
+        Type: 'CSS',
+        Content: '',
+    },
+    {
+        key: 3 ,
+        Type: 'React',
+        Content: '',
+    },
+    {
+        key: 4 ,
+        Type: 'React Native',
+        Content: '',
+    },
+    {
+        key: 5 ,
+        Type: 'Next.js',
+        Content: '',
+    },
+    {
+        key: 6 ,
+        Type: 'Node.js',
+        Content: '',
+    },
+    {
+        key: 7 ,
+        Type: 'MySQL',
+        Content: '',
+    },
+    {
+        key: 8 ,
+        Type: 'Tailwind',
+        Content: '',
+    },
+    {
+        key: 9 ,
+        Type: 'Java Scripts',
+        Content: '',
+    },
+    {
+        key: 10 ,
+        Type: 'Less',
+        Content: '',
+    },
+    {
+        key: 11 ,
+        Type: 'Sass',
+        Content: '',
+    },
+];
