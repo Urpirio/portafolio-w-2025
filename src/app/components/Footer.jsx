209730781413de9d7681b2a4b93d1be7411536ad
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-04-13 09:34:08 -0400

    Se agrego la dependencia de los iconos

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -1,4 +1,6 @@
-import React from 'react';
+import { FaLinkedin } from "react-icons/fa";
+import { RiInstagramFill } from "react-icons/ri";
+import { IoLogoGithub } from "react-icons/io";
 import LinkLinkedin from '../func/LinkLinkedin';
 import LinkInstagram from '../func/LinkInstagram';
 import LinkGithud from '../func/LinkGithud';
@@ -15,9 +17,9 @@ export default function Footer() {
             <div className='flex flex-col w-[30%] min-w-80 items-center gap-2 '>
               <h3 className='text-2xl font-bold text-green-500'>Social networks</h3>
               <div className='flex gap-5'>
-                <i onClick={LinkLinkedin} class='bx bxl-linkedin-square text-4xl text-white cursor-pointer transition-all duration-200 hover:scale-110 hover:text-green-500' ></i>
-                <i onClick={LinkInstagram} class='bx bxl-instagram-alt text-4xl text-white cursor-pointer transition-all duration-200 hover:scale-110 hover:text-green-500 ' ></i>
-                <i onClick={LinkGithud} class='bx bxl-github text-4xl text-white cursor-pointer transition-all duration-200 hover:scale-110 hover:text-green-500' ></i>
+                <FaLinkedin onClick={LinkLinkedin} className='text-4xl text-white cursor-pointer transition-all duration-200 hover:scale-110 hover:text-green-500'/>
+                <RiInstagramFill  onClick={LinkInstagram} className="text-4xl text-white cursor-pointer transition-all duration-200 hover:scale-110 hover:text-green-500" />
+                <IoLogoGithub onClick={LinkGithud} className='text-4xl text-white cursor-pointer transition-all duration-200 hover:scale-110 hover:text-green-500'/>
               </div>
             </div>
           </section>
