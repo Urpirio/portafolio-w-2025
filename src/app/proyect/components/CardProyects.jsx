ed7e9565e831065cfedd28053a38ce35e7fbeb3d
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-04-13 09:40:21 -0400

    Se cambio la manera de renderizar los componentes y se optimizo el codigo

--- src/app/proyect/components/CardProyects.jsx
+++ src/app/proyect/components/CardProyects.jsx
@@ -1,46 +1,15 @@
 'use client';
-import React from 'react';
-import DataProyects from '../Data/DataProyects';
-import SubCardProyect from './Subcomponents/SubCardProyect';
 import { usePathname } from 'next/navigation';
-import DataProyects2 from '../Data/DataProyects2';
-
+import { ProyectsPage1, ProyectsPage2 } from './Subcomponents/SubCardProyect';
 
 export default function CardProyects() {
 
     const path = '/proyect/page2' == usePathname();
 
-    const proyectlist = DataProyects.map( DP => {
-        return(
-          <SubCardProyect 
-          img = {DP.Img[0].D} 
-          NameP = {DP.NameP}
-          Description = {DP.Descripcion}
-          id = {DP.id}
-          LinkDeploy = {DP.LinkDeploy}
-          LinkGithud = {DP.LinkGithud}
-           />
-        )
-      });
-
-      const proyectlist2 = DataProyects2.map( DP => {
-        return(
-          <SubCardProyect 
-          img = {DP.Img[0].D} 
-          NameP = {DP.NameP}
-          Description = {DP.Descripcion}
-          id = {DP.id}
-          LinkDeploy = {DP.LinkDeploy}
-          LinkGithud = {DP.LinkGithud}
-           />
-        )
-      });
-
-
   return (
     <div className=' flex flex-wrap min-h-120 justify-center items-center gap-5'> 
                 <span className='hidden' id='RenderProyects'>No results found</span>
-        {path ? proyectlist2 : proyectlist}
+        {path ? <ProyectsPage2/> : <ProyectsPage1/>}
       </div>
   )
 }
