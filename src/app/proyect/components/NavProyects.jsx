b89c88e303f5459d204dfac976eed0086297c6e6
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-04-13 09:40:50 -0400

    Se cambio la manera de mapear los componentes y se optimizo el codigo

--- src/app/proyect/components/NavProyects.jsx
+++ src/app/proyect/components/NavProyects.jsx
@@ -1,33 +1,12 @@
 'use client';
-
-import React from 'react';
-import { useState } from 'react';
-import Link from 'next/link';
-import { usePathname } from 'next/navigation';
-import LinkProyectNav from '../func/LinkProyectNav';
-import SubNavProyects from './Subcomponents/SubNavProyects';
-import DataProyectsPage from '../Data/DataProyectsPage';
 import BtnNextPage from './Subcomponents/BtnNextPage';
 import BtnReturnPage from './Subcomponents/BtnReturnPage';
+import SubNavProyects from './Subcomponents/SubNavProyects';
 
 
 export let IsPageTwo;
 export default function NavProyects() {
 
-    const pathBoleano = '/proyect/page2' == usePathname();
-    const pathBoleano2 = '/proyect' == usePathname();
-
-    const YouAreHere = DataProyectsPage.map(DPP => {
-        return(
-            <SubNavProyects
-            Page = {DPP.Page}
-            NextPage = {DPP.NextPage}
-            LinkPage = {DPP.LinkPage}
-            LinkNextPage = {DPP.LinkNextPage}
-            path = {usePathname()}
-            />
-        )
-    })
 
 
 
@@ -36,7 +15,7 @@ export default function NavProyects() {
   return (
     <div className='flex items-center gap-5'>
        <BtnReturnPage/>
-        {YouAreHere}
+       <SubNavProyects/>
        <BtnNextPage/>
   </div>
   )
