6fb6000713c8c6d881738a2434376257aa00a2bb
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-04-13 09:46:07 -0400

    Se agrego de maera interna el Array de caracteristicas de servicios

--- src/app/Services/Data/DataCardServices.js
+++ src/app/Services/Data/DataCardServices.js
@@ -1,24 +1,81 @@
 const DataCardServices = [
     {
+        key: 1 ,
         ServiceName: 'SimplePage',
         DescriptionService: 'A simple, single-page website with smooth navigation, responsive design, and basic SEO optimization.',
         StandarPrice: 'US$ 110',
         id_Caracteristicas_Services: 'Card1',
-        CardID: 'CardServices0'
+        CardID: 'CardServices0',
+        Feature:  [
+            {
+                key: 1 ,
+                service: '✔️Attractive and responsive design.',
+            },
+            {
+                key: 2 ,
+                service: '✔️Basic SEO optimization.',
+            },
+            {
+                key: 3 ,
+                service: '✔️Contact forms.',
+            },
+            {
+                key: 4 ,
+                service: '✔️Smooth animations and interactive effects.',
+            }
+        ],
     },
     {
+        key: 2 ,
         ServiceName: 'MultiPage',
         DescriptionService: 'A multi-page static website with clear navigation, professional design, and SEO-friendly structure.',
         StandarPrice: 'US$ 220',
         id_Caracteristicas_Services: 'Card2',
         CardID: 'CardServices1',
+        Feature:[
+            {
+                key: 1 ,
+                service: '✔️Design of multiple static pages (home, services, contact, etc.).',
+            },
+            {
+                key: 2 ,
+                service: '✔️Easy-to-use navigation.',
+            },
+            {
+                key: 3 ,
+                service: '✔️Basic SEO optimization.',
+            },
+            {
+                key: 4 ,
+                service: '✔️Responsive design.',
+            }
+        ],
     },
     {
+        key: 3 ,
         ServiceName: 'DynamicPage',
         DescriptionService: 'An interactive, database-driven website with user authentication, dynamic content, and custom features.',
         StandarPrice: 'US$ 500',
         id_Caracteristicas_Services: 'Card3',
         CardID: 'CardServices2',
+        Feature:[
+            {
+                key: 1 ,
+                service: '✔️Development with backend (Node.js).',
+            },
+            {
+                key: 2 ,
+                service: '✔️Integration with databases (MySQL).',
+            },
+            {
+                key: 3 ,
+                service: '✔️Interactive features (login, comments, purchases).',
+            },
+            {
+                key: 4 ,
+                service: '✔️Advanced SEO optimization and security.',
+            }
+        ]
     }
 ];
 export default DataCardServices;
