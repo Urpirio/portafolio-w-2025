130c5e985f40d0db5be845995915abbc16b6ef64
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-04-13 09:42:55 -0400

    Se optimizo el codigo haciendo que el mapeo d eun array de dato se haga de manera interna

--- src/app/proyect/components/Subcomponents/SubNavProyects.jsx
+++ src/app/proyect/components/Subcomponents/SubNavProyects.jsx
@@ -1,19 +1,28 @@
-import React from 'react';
 import { usePathname } from 'next/navigation';
 import LinkProyectNav from '../../func/LinkProyectNav';
+import DataProyectsPage from '../../Data/DataProyectsPage';
 
-export default function SubNavProyects(props) {
+export default function SubNavProyects() {
 
-    const pathBoleano = '/proyect/page2' == usePathname();
-    const pathBoleano2 = '/proyect' == usePathname();
 
-  return (
-    <div className='flex gap-2'>
-        <button onClick={()=>{LinkProyectNav(props.LinkPage,props.LinkNextPage,props.path)}} className={`flex justify-center items-center h-10 w-10 border-1 p-1 rounded-xl  shadow-xs cursor-pointer transition-all duration-200 hover:opacity-75 
-        ${pathBoleano2 ? 'bg-green-500 text-white' : 'border-gray-200 bg-gray-100 text-gray-600' }`}>{props.Page}</button>
+  const pathBoleano = '/proyect/page2' == usePathname();
+  const pathBoleano2 = '/proyect' == usePathname();
+  const Path = usePathname();
+  
+  const ProyectBtns = DataProyectsPage.map(DPP => {
 
-        <button onClick={()=>{LinkProyectNav(props.LinkPage,props.LinkNextPage,props.path)}} className={`flex justify-center items-center h-10 w-10 border-1  p-1 rounded-xl  shadow-xs cursor-pointer transition-all duration-200 hover:opacity-75 
-        ${pathBoleano ? 'border-green-500 bg-green-500 text-white' : 'border-gray-200 bg-gray-100 text-gray-600' }`}>{props.NextPage}</button>
-    </div>
-  )
-}
+      return(
+        <div key={DPP.key} className='flex gap-2'>
+          <button onClick={()=>{LinkProyectNav(DPP.LinkPage,DPP.LinkNextPage,Path)}} className={`flex justify-center items-center h-10 w-10 border-1 p-1 rounded-xl  shadow-xs cursor-pointer transition-all duration-200 hover:opacity-75 
+          ${pathBoleano2 ? 'bg-green-500 text-white' : 'border-gray-200 bg-gray-100 text-gray-600' }`}>{DPP.Page}</button>
+
+          <button onClick={()=>{LinkProyectNav(DPP.LinkPage,DPP.LinkNextPage,Path)}} className={`flex justify-center items-center h-10 w-10 border-1  p-1 rounded-xl  shadow-xs cursor-pointer transition-all duration-200 hover:opacity-75 
+          ${pathBoleano ? 'border-green-500 bg-green-500 text-white' : 'border-gray-200 bg-gray-100 text-gray-600' }`}>{DPP.NextPage}</button>
+        </div>
+      )
+  });
+
+  return(ProyectBtns)
+
+ 
+};
