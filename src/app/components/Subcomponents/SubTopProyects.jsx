6d9413c4a9b5545553ebf043f6761e004bc2c0dd
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-04-13 09:37:28 -0400

    Se mododifico el componente por completo para que se mapeara el contenido dentro del mismo

--- src/app/components/Subcomponents/SubTopProyects.jsx
+++ src/app/components/Subcomponents/SubTopProyects.jsx
@@ -1,22 +1,28 @@
 'use client';
 
-import React from 'react';
 import Image from 'next/image';
+import DataTopProyects from '@/app/Data/DataTopProyects';
 
-export default function SubTopProyects(props) {
+export default function SubTopProyects() {
 
-  const LinkProyects = () => {
-    window.location.href = props.link;
-  };
+  
+  const TopList = DataTopProyects.map(DTP => {
 
-
-  return (
-    <div onClick={LinkProyects} className='flex flex-col justify-center items-center w-100 p-2 gap-3  border-2  border-green-400 rounded-2xl shadow-2xl  cursor-pointer transition-all duration-100 hover:shadow-green-200  hover:scale-105'>
-        <Image className='h-50 w-[100%] rounded-xl object-cover ' src={props.Image} alt={props.NameP}/>
+    const LinkProyects = () => {
+      window.location.href = DTP.Link;
+    };
+  
+    return(
+      <div key={DTP.Key} onClick={LinkProyects} className='flex flex-col justify-center items-center w-100 p-2 gap-3  border-2  border-green-400 rounded-2xl shadow-2xl  cursor-pointer transition-all duration-100 hover:shadow-green-200  hover:scale-105'>
+        <Image className='h-50 w-[100%] rounded-xl object-cover ' src={DTP.Img[0].D} alt={DTP.NameP}/>
         <div>
             <h3 className='text-green-950 font-bold'>{props.NameP}</h3>
             <span>{props.description}</span>
+            <span>{DTP.Descripcion}</span>
         </div>
-    </div>
-  )
-}
+      </div>
+    )
+  })
+
+  return (TopList)
+};
