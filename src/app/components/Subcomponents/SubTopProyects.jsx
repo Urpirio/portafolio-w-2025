414346ff65246c7e8335789f99f4fb5c18314396
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-04-13 09:38:02 -0400

    No hubo cambios relevantes

--- src/app/components/Subcomponents/SubTopProyects.jsx
+++ src/app/components/Subcomponents/SubTopProyects.jsx
@@ -16,8 +16,7 @@ export default function SubTopProyects() {
       <div key={DTP.Key} onClick={LinkProyects} className='flex flex-col justify-center items-center w-100 p-2 gap-3  border-2  border-green-400 rounded-2xl shadow-2xl  cursor-pointer transition-all duration-100 hover:shadow-green-200  hover:scale-105'>
         <Image className='h-50 w-[100%] rounded-xl object-cover ' src={DTP.Img[0].D} alt={DTP.NameP}/>
         <div>
-            <h3 className='text-green-950 font-bold'>{props.NameP}</h3>
-            <span>{props.description}</span>
+            <h3 className='text-green-950 font-bold'>{DTP.NameP}</h3>
             <span>{DTP.Descripcion}</span>
         </div>
       </div>
