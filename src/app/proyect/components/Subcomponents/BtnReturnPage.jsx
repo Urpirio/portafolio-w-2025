87c9bb8a0068b36ebc4eda8122f9cb5f4e59eb52
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-04-13 09:41:47 -0400

    Se elimino la importacion de react y agrego un Icono de react Icons

--- src/app/proyect/components/Subcomponents/BtnReturnPage.jsx
+++ src/app/proyect/components/Subcomponents/BtnReturnPage.jsx
@@ -1,5 +1,5 @@
 'use client';
-import React from 'react';
+import { IoIosArrowBack } from "react-icons/io";
 import { usePathname } from 'next/navigation';
 import DataProyectsPage from '../../Data/DataProyectsPage';
 
@@ -21,7 +21,7 @@ export default function BtnReturnPage() {
     <div>
         <button onClick={Direction}  className={`h-6 w-6 flex justify-center items-center border-1 rounded-sm cursor-pointer transition-all duration-200 hover:opacity-75
             ${path ? 'border-gray-200 rounded-xs bg-gray-200 text-gray-600' : 'bg-green-500 text-white'}`} id='ReturnPastProyects'>
-                <i className='bx bx-chevron-left' ></i>
+                <IoIosArrowBack/>
         </button>
     </div>
   )
