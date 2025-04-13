ccdf3a93e5e7fd92c580214c15de27ff1c51d4e1
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-04-13 09:42:21 -0400

    se optimizo el contigo, haciendo que el mapeo del arrray de datos se haga de manera interna

--- src/app/proyect/components/Subcomponents/SubCardProyect.jsx
+++ src/app/proyect/components/Subcomponents/SubCardProyect.jsx
@@ -1,22 +1,51 @@
-import React from 'react';
 import Image from 'next/image';
 import LinkGithud from '@/app/func/LinkGithud';
 import LinkDeploy from '../../func/LinkProyects/LinkDeploy';
+import { IoLogoGithub } from "react-icons/io";
+import DataProyects from '../../Data/DataProyects';
+import DataProyects2 from '../../Data/DataProyects2';
 
-export default function SubCardProyect(props) {
-  return (
-    <article className='flex flex-col gap-2 shadow-xl border-2 border-gray-300 rounded-xl ' id={props.id}>
+
+export const ProyectsPage1 = () => {
+  const Proyects = DataProyects.map( DP => {
+    return(
+      <article key={DP.key}  className='flex flex-col gap-2 shadow-xl border-2 border-gray-300 rounded-xl ' id={DP.id}>
         <div className='w-80 sm:w-[100%] sm:min-w-100'>
-            <Image src={props.img} className='h-40 sm:w-[100%] w-80 sm:h-50 border-b-1 border-gray-200 rounded-t-xl object-cover'/>
+            <Image src={DP.Img[0].D} alt={DP.NameP} className='h-40 sm:w-[100%] w-80 sm:h-50 border-b-1 border-gray-200 rounded-t-xl object-cover'/>
         </div>
         <div className='p-2'>
-            <h2 className='font-bold'>{props.NameP}</h2>
-            <p className='text-[14px] text-gray-500'>{props.Description}</p>
-        </div>
+          <h2 className='font-bold'>{DP.NameP}</h2>
+          <p className='text-[14px] text-gray-500'>{DP.Descripcion}</p>
+         </div>
         <div className='w-[100%]  p-2 flex justify-center gap-2'>
-            <button onClick={()=>{LinkDeploy(props.LinkDeploy)}} className=' h-10 w-[85%] border-1 rounded-xl bg-green-500 text-white shadow-2xs cursor-pointer transition-all duration-200 hover:opacity-75'>See project</button>
-            <button onClick={()=>{LinkGithud(props.LinkGithud)}} className='flex justify-center items-center h-10 w-[15%] border-1 rounded-xl bg-gray-900 text-white shadow-2xs cursor-pointer text-2xl transition-all duration-200 hover:opacity-75'><i class='bx bxl-github'></i></button>
+          <button onClick={()=>{LinkDeploy(DP.LinkDeploy)}} className=' h-10 w-[85%] border-1 rounded-xl bg-green-500 text-white shadow-2xs cursor-pointer transition-all duration-200 hover:opacity-75'>See project</button>
+          <button onClick={()=>{LinkGithud(DP.LinkGithud)}} className='flex justify-center items-center h-10 w-[15%] border-1 rounded-xl bg-gray-900 text-white shadow-2xs cursor-pointer text-2xl transition-all duration-200 hover:opacity-75'><IoLogoGithub/></button>
         </div>
     </article>
-  )
-}
+    )
+  })
+
+  return(Proyects)
+} 
+
+export const ProyectsPage2 = () => {
+  const Proyects = DataProyects2.map( DP => {
+    return(
+      <article key={DP.key} className='flex flex-col gap-2 shadow-xl border-2 border-gray-300 rounded-xl ' id={DP.id}>
+        <div className='w-80 sm:w-[100%] sm:min-w-100'>
+            <Image src={DP.Img[0].D} alt={DP.NameP} className='h-40 sm:w-[100%] w-80 sm:h-50 border-b-1 border-gray-200 rounded-t-xl object-cover'/>
+        </div>
+        <div className='p-2'>
+          <h2 className='font-bold'>{DP.NameP}</h2>
+          <p className='text-[14px] text-gray-500'>{DP.Descripcion}</p>
+         </div>
+        <div className='w-[100%]  p-2 flex justify-center gap-2'>
+          <button onClick={()=>{LinkDeploy(DP.LinkDeploy)}} className=' h-10 w-[85%] border-1 rounded-xl bg-green-500 text-white shadow-2xs cursor-pointer transition-all duration-200 hover:opacity-75'>See project</button>
+          <button onClick={()=>{LinkGithud(DP.LinkGithud)}} className='flex justify-center items-center h-10 w-[15%] border-1 rounded-xl bg-gray-900 text-white shadow-2xs cursor-pointer text-2xl transition-all duration-200 hover:opacity-75'><IoLogoGithub/></button>
+        </div>
+    </article>
+    )
+  })
+
+  return(Proyects)
+} 
