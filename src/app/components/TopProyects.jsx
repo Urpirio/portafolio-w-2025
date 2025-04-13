82763b35246460995d6384d3aab217536119a578
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-04-13 09:38:38 -0400

    Se elimino el componente mas el map y ahora se mapea dentro del mismo componente

--- src/app/components/TopProyects.jsx
+++ src/app/components/TopProyects.jsx
@@ -1,26 +1,15 @@
 'use client';
 
-import React from 'react';
 import SubTopProyects from './Subcomponents/SubTopProyects';
-import DataTopProyects from '../Data/DataTopProyects';
 import Linkproyect from '../func/LinkProyects';
 
 export default function TopProyects() {
-    const TopListProyects = DataTopProyects.map(DTP => {
-        return(
-            <SubTopProyects
-            Image = {DTP.Img[0].D}
-            NameP = {DTP.NameP}
-            description = {DTP.Descripcion}
-            link = {DTP.Link}
-            />
-        )
-    });
+    
 
   return (
     <div className='flex flex-col items-center  gap-10 p-1 sm:p-5 ' >
         <div className='flex flex-wrap justify-center gap-5 w-[90%]'>
-        {TopListProyects}
+        <SubTopProyects/>
         </div>
         <div>
             <button onClick={()=>{Linkproyect(true)}} className='flex justify-center items-center h-10 p-2 border-1 rounded-[10px] font-bold bg-green-500 text-white cursor-pointer transition-all duration-200 hover:translate-y-[-10px] hover:opacity-75'>See more projects</button>
