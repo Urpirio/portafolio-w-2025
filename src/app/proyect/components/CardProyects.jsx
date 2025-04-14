5498fa186ec86c2d3286229194fe97db4d29cc8d
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-04-13 21:50:59 -0400

    Se restructuro el componente de cartas para resolver un pequeño error en el diseño causado por el buscador

--- src/app/proyect/components/CardProyects.jsx
+++ src/app/proyect/components/CardProyects.jsx
@@ -7,9 +7,13 @@ export default function CardProyects() {
     const path = '/proyect/page2' == usePathname();
 
   return (
-    <div className=' flex flex-wrap min-h-120 justify-center items-center gap-5'> 
-                <span className='hidden' id='RenderProyects'>No results found</span>
-        {path ? <ProyectsPage2/> : <ProyectsPage1/>}
+    <div className='flex flex-col justify-center items-center' id='ContainerProyects'> 
+        <div className='hidden justify-center items-center' id='RenderProyects'>
+         <span >No results found</span>
+        </div>       
+        <div className='flex flex-wrap min-h-120 justify-center items-center gap-5'>
+          {path ? <ProyectsPage2/> : <ProyectsPage1/>}
+        </div>
       </div>
   )
 }
--- src/app/proyect/components/SearchProyects.jsx
+++ src/app/proyect/components/SearchProyects.jsx
@@ -1,5 +1,5 @@
 'use client'
-import React from 'react';
+import { GoSearch } from "react-icons/go";
 import Search from '../func/Search';
 import { usePathname } from 'next/navigation';
 
@@ -8,7 +8,7 @@ export default function SearchProyects() {
   return (
     <div className='w-[100%] flex justify-center'>
         <span className='flex items-center justify-center border-1 border-gray-400 rounded-2xl h-10 w-[50%]  min-w-80 gap-1 '>
-          <i className='bx bx-search text-2xl text-gray-400' ></i>
+          <GoSearch className="text-2xl text-gray-400"/>
           <input onInput={()=>{Search(path)}} id="Search1" className='w-[90%] h-[100%]  pl-2 pr-2 outline-none ' type="text" name=""  placeholder='Search proyect'/>
           <i className='bx bx-x text-2xl' style={{display: 'none'}}></i>
         </span>
