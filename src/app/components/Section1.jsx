24719085691cd413610f42dd20c21ea913744d36
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-07-25 15:22:59 -0400

    Update

--- src/app/components/Section1.jsx
+++ src/app/components/Section1.jsx
@@ -28,8 +28,8 @@ export default function Section1() {
     </div>
     <div className='flex flex-col items-center w-[100%] gap-2'>
       <h1 className=' font-extrabold text-4xl sm:text-5xl text-center'>Hey, I'm  Urpirio Junior</h1>
-      <p className=' text-xl sm:text-2xl text-gray-600 text-center'>A results-driven Web Developer
-      focused on building and optimizing websites and web applications to enhance user experience and drive product success.</p>
+      <p className=' text-xl sm:text-2xl text-gray-600 text-center'>A results-driven Mobile Developer and Web
+      focused on building and optimizing websites and web applications,Mobile application to enhance user experience and drive product success.</p>
     </div>
     <div className='w-[100%] flex justify-center items-center' id='Aboutme'>
       <button onClick={()=>{Linkproyect(false)}} className='bg-green-500 shadow-3xl h-12 w-40 text-white rounded-xl text-[18px] cursor-pointer transition-all duration-300 hover:opacity-75'>Proyects</button>
