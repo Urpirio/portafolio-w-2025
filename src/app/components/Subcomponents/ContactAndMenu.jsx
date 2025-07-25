d51df332f8be72dc65cd9e059e0d11d08733ffcf
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-07-25 15:05:11 -0400

    Se optimizo el codigo

--- src/app/components/Subcomponents/ContactAndMenu.jsx
+++ src/app/components/Subcomponents/ContactAndMenu.jsx
@@ -1,38 +1,34 @@
-'use client'
+"use client";
 import { TfiMenu } from "react-icons/tfi";
 import { IoMdClose } from "react-icons/io";
-import { usePathname } from 'next/navigation';
-import LinkContact from '@/app/func/LinkContact';
-import { Deploymenu } from "./func/Deploymenu";
-import { IsDeployMovilMenu } from "./func/Deploymenu";
+import { usePathname } from "next/navigation";
+import LinkContact from "@/app/func/LinkContact";
 import { useEffect, useState } from "react";
 
+export default function ContactAndMenu({setDeployMenu,DeployMenu}) {
+  //componente del menu
 
-
-
-export default function ContactAndMenu() {//componente del menu
-
-  const [Prueba,setPrueba] = useState(<TfiMenu/>)
+  const [Prueba, setPrueba] = useState(<TfiMenu />);
   const pathBoleano = usePathname();
 
-  useEffect(()=>{
-
-    const IsDeploy = () =>{
-      if(IsDeployMovilMenu[0].Boolean === false){
-        setPrueba(<TfiMenu/>)
-      }else{
-        setPrueba(<IoMdClose/>)
-      }
-    };
-    IsDeploy();
-  },[])
-
   return (
-    <div className='flex flex-grow basis-0 justify-end' >
-        <button onClick={()=>{LinkContact(pathBoleano)}} className=' hidden sm:flex h-10 p-5  border-1 justify-center items-center rounded-2xl shadow-2xl shadow-green-200 bg-green-500 text-white font-bold cursor-pointer hover:opacity-75 '>Contact</button>
-        <button name="Navegation Menu" onClick={()=>{Deploymenu(true)}} className='text-4xl sm:hidden flex' id='MenuNav'>
-          {Prueba}
-        </button>
+    <div className="flex flex-grow basis-0 justify-end">
+      <button
+        onClick={() => {
+          LinkContact(pathBoleano);
+        }}
+        className=" hidden sm:flex h-10 p-5  border-1 justify-center items-center rounded-2xl shadow-2xl shadow-green-200 bg-green-500 text-white font-bold cursor-pointer hover:opacity-75 "
+      >
+        Contact
+      </button>
+      <button
+        onClick={()=>setDeployMenu(!DeployMenu)}
+        name="Navegation Menu"
+        className="text-4xl sm:hidden flex"
+        id="MenuNav"
+      >
+        {DeployMenu ? <IoMdClose /> : <TfiMenu />}
+      </button>
     </div>
-  )
+  );
 }
