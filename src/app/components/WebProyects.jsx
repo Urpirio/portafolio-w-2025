82cd19ed424b366e3b9fc54f3a3f5270a40c7e95
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-07-25 08:49:08 -0400

    Se le cambio el nombre a la seccion

---
+++ src/app/components/WebProyects.jsx
@@ -0,0 +1,11 @@
+
+export default function Proyects() {
+  return (
+    <section className='flex flex-col items-center p-10' >
+        <div className='flex flex-col items-center gap-3 '>
+            <h2 className=' text-4xl'>Web Proyects</h2>
+            <div className='h-1.5  bg-green-400 w-7 rounded-2xl'></div>
+        </div>
+    </section>
+  )
+}
