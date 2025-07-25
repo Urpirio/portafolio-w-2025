9cb4c442d2a672433a2a5dd49fafd1deec9a3bcc
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-07-25 08:48:49 -0400

    Se agrego un array de datos con laa aplicaciones desarrolladas

---
+++ src/app/components/Subcomponents/Data/DataMobileProyects.js
@@ -0,0 +1,23 @@
+import ImgDiggetApp from '@/../public/Img-MobileProyects/8751.jpg';
+import ImgDI2025  from '@/../public/Img-MobileProyects/8752.jpg';
+import ImgChatGpt  from '@/../public/Img-MobileProyects/8753.jpg'
+import ImgAppTienda from '@/../public/Img-MobileProyects/Store.jpg'
+
+export const DataMobileProyects = [
+    {
+        Title:'Clon-ChatGpt',
+        Image: ImgChatGpt,
+    },
+    {
+        Title:'Digget-App',
+        Image: ImgDiggetApp,
+    },
+    {
+        Title:'DI-2025',
+        Image: ImgDI2025,
+    },
+    {
+        Title:'Prototipo-App',
+        Image: ImgAppTienda,
+    }
+]
