b965e12e445ac0c2472c655880f11f37feda5071
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-07-25 15:05:54 -0400

    Se cambio le layout

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,28 +1,41 @@
-'use client';
-import '../styles/Globals.css';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import { Deploymenu } from './components/Subcomponents/func/Deploymenu';
+"use client";
+import "../styles/Globals.css";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import { useState } from "react";
+
+export default function Layout({ children }) {
+
+  const [DeployMenu,setDeployMenu] = useState(false);
 
 
-export default function Layout({children}) {
- 
   return (
-
     <html lang="en">
-    <head>
+      <head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta name="description" content="I’m Urpirio Junior Moreno Vargas, a Full Stack Developer with 
+        <meta
+          name="description"
+          content="I’m Urpirio Junior Moreno Vargas, a Full Stack Developer with 
         over a year of experience in web development. I use technologies like React, Node.js, and MySQL
         to build scalable, efficient applications with great user experiences. Explore my projects and 
-        get in touch to collaborate." />
+        get in touch to collaborate."
+        />
         <title>UrpirioDev</title>
-    </head>
-    <body onTouchMove={()=>{Deploymenu(false)}} >
-      <Header/>
+        <link
+          rel="shortcut icon"
+          href="https://www.urpiriodev.online/_next/image?url=%2F_next%2Fstatic%2Fmedia%2FLogoPortafolio.11f8eaae.png&w=1920&q=75"
+          type="image/x-icon"
+        />
+      </head>
+      <body
+        onTouchMove={() => {
+          setDeployMenu(false);
+        }}
+      >
+        <Header setDeployMenu={setDeployMenu} DeployMenu={DeployMenu} />
         {children}
-      <Footer/>
-    </body>
+        <Footer />
+      </body>
     </html>
-  )
+  );
 }
